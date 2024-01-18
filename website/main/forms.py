81ecc4fa7c7ad0b18eb7a3c8ce8ed6371560b3df
Ernest Perżyło <eperzylo123@gmail.com>
2024-01-18 20:31:43 +0100

    login-style

--- website/main/forms.py
+++ website/main/forms.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
@@ -10,8 +11,23 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+    
+        
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.fields['username'].label = 'Nazwa użytkownika'
+        self.fields['password1'].label = 'Hasło'
+        self.fields['password2'].label = 'Powtórz hasło'
+
 
 class LoginForm(AuthenticationForm):
 
     username = forms.CharField(widget=TextInput())
     password = forms.CharField(widget=PasswordInput())
+
+    def __init__(self, request: Any = ..., *args: Any, **kwargs: Any) -> None:
+        super().__init__(request, *args, **kwargs)
+
+        self.fields['username'].label = 'Nazwa użytkownika'
+        self.fields['password'].label = 'Hasło'
